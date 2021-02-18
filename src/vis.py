1b86d049d22cbaf60719b90fcabffa36c224744d
Dave Morrissey <dmor0005@student.monash.edu>
2021-02-18 17:33:31 +1100

    added author info

--- src/vis.py
+++ src/vis.py
@@ -10,6 +10,16 @@ st.set_page_config(page_title='Sales Data Sample Dashboard',
                    layout='wide',
                    initial_sidebar_state='auto')
 
+st.markdown('''
+# Sales Data Sample Dashboard
+
+By David Morrissey 18/02/2021.
+
+A simple demo of what is possible with sales data and powerful data analysis tools such as 
+(streamlit)[https://streamlit.io], (pandas)[https://pandas.pydata.org/], (plotly dash)[https://plotly.com/dash/] 
+and (pydeck)[https://deckgl.readthedocs.io/en/latest/].
+''')
+
 df = get_sales_data()
 st.sidebar.header('Page Navigation')
 st.sidebar.markdown('''
